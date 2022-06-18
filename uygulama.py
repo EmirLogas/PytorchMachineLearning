ddab2d56f80a28e88902136b994019e992b112dd
Emir <76751818+EmirLogas@users.noreply.github.com>
2022-06-18 18:48:09 +0300

    Update

--- uygulama.py
+++ uygulama.py
@@ -1,120 +1,106 @@
-import torch
+import cv2
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 from torchvision.transforms import ToTensor, Resize
-from torch.utils.data import DataLoader, Dataset
-import glob
-import cv2
-import random
-
-class MyDataset(Dataset):
-    def __init__(self, train):
-        # Veri kümesinin yolu
-        data_path = "/dataset"
-
-        # Veri kümesine ait klasördeki her bir JPG uzantılı resmin dosya yolu bir listeye kaydedildi.
-        self.data_list = glob.glob(data_path + "/*/*.JPG")
-
-        # Liste karıştırıldı.
-        random.shuffle(self.data_list)
-
-        # train True ise eğitim
-        # train False ise doğrulama verim kümesi olacak şekilde ayarlandı.
-        if train == True:
-            self.data_list = self.data_list[:220]
-        else:
-            self.data_list = self.data_list[220:]
-
-    def __getitem__(self, index):
-        # görüntü dosyadan okunur.
-        image = cv2.imread(self.data_list[index])
-
-        # görüntü pytorch tensor veri tipine dönüştürülür ve 64x64 boyutuna indirgenir
-        image = ToTensor()(image)
-        image = Resize((64,64))(image)
-
-        # dosya yolu bilgisinden etiket değeri elde edilir.
-        label = self.data_list[index].split('/')[4]
-        return image, int(label)
-  
-    def __len__(self):
-        return len(self.data_list)
-
-ds_train = MyDataset(train = True)
-ds_val = MyDataset(train = False)
-
-dl_train = DataLoader(ds_train,batch_size=32,shuffle=True)
-dl_val = DataLoader(ds_val,batch_size=32,shuffle=True)
 
 class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv_layer1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
-        self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3,padding=1)
-        self.max_pool1 = nn.MaxPool2d(kernel_size = 2, stride = 2)
-        
-        self.conv_layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3,padding=1)
-        self.conv_layer4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3,padding=1)
-        self.max_pool2 = nn.MaxPool2d(kernel_size = 2, stride = 2)
-        
-        self.fc1 = nn.Linear(16384, 1000)
-        self.fc2 = nn.Linear(1000, 10)
-    
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.pool4 = nn.MaxPool2d(2, 2)
+
+        self.dropout = nn.Dropout(0.25)
+
+        self.fc1 = nn.Linear(64 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, 2)
+
     def forward(self, x):
-        out = self.conv_layer1(x)
+        out = self.conv1(x)
         out = F.relu(out)
-        out = self.conv_layer2(out)
+        out = self.pool(out)
+
+        out = self.conv2(out)
         out = F.relu(out)
-        out = self.max_pool1(out)
-        
-        out = self.conv_layer3(out)
+        out = self.pool2(out)
+
+        out = self.conv3(out)
         out = F.relu(out)
-        out = self.conv_layer4(out)
+        out = self.pool3(out)
+
+        out = self.conv4(out)
         out = F.relu(out)
-        out = self.max_pool2(out)
-                
-        out = out.reshape(out.size(0), -1)
-        
+        out = self.pool4(out)
+
+        out = out.view(out.size(0), -1)
+
+        out = self.dropout(out)
         out = self.fc1(out)
+
         out = F.relu(out)
+
+        out = self.dropout(out)
         out = self.fc2(out)
+
         return out
 
 model = MyModel()
 
-# kayıp fonksiyonu
-loss_fn = nn.CrossEntropyLoss()
+model.load_state_dict(torch.load('model_weights3.pth'))
+model.eval()
 
-# Optimize edici olarak stochastic gradient descent seçtik, parametre olarak 
-# model parametrelerini ve öğrenme oranını alır
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+# Resimleri okumak için gerekli olan transform
+image = cv2.imread('asd.jpeg')
+image = cv2.resize(image, (64, 64))
+image = ToTensor()(image)
+image = image.unsqueeze(0)
 
-def dogrulama():
-    correct = 0
-    with torch.no_grad():  # İleri yayılımda gradyanların hesaplanmaması için kullanılır. Gradyan hesabı eğitim için gerekli. gradyan hesabının yapılmaması için test aşamasında bu yapıyı kullanıyoruz.
-        for data, label in dl_val:
-            tahmin = model(data())   # görüntüler modelden geçirilir ve tahmin değerleri elde edilir.
-            correct += (tahmin.argmax(1) == label()).type(torch.float).sum().item()  # kaç tahmin doğru olarak bulundu
+with torch.no_grad():
+    output = model(image)
 
-    sonuc = correct / len(ds_val)  # doğru olarak bulunan tahminlerin toplam veri sayısına oranı
-    return sonuc
+print(output.argmax().item())
 
-epoch = 50
 
-for idx in range(epoch):
-    for data, label in dl_train:
-        # her iterasyonda batch_size miktarınca görüntü modelden geçirilerek her 
-        # görüntü için tahmin değerleri hesaplanıyor.
-        tahmin = model(data)
+# WEBCAM
+"""vid = cv2.VideoCapture(0)
 
-        # elde edilen tahmin değerlerinin olması gereken değerlerden ne kadar uzak 
-        # olduğuna dair kayıp hesaplanıyor.
-        loss = loss_fn(tahmin, label)
+while(True):
+    # her itarasyonda webcam'dan bir görüntü okutulur.
+    ret, frame = vid.read()
 
-        # Backpropagation (geri yayılım ve ağırlıkların güncellenmesi )
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    # görüntü modelin kabul edeceği şekle dönüştürülür
+    im = ToTensor()(frame)
+    im = Resize((64, 64))(im)
+    im = im.unsqueeze(0)
 
-    # Her epoch'ta kullanıcı bilgilendiriliyor.
-    print("Epoch :",idx, "Loss :", loss.item(), "val acc", dogrulama())
+    # görüntü model tarafından test edilir.
+    with torch.no_grad():
+        model_out = model(im)
+
+    # Elde edilen sonuç terminale yazdırılır.
+    print(model_out.argmax().item())
+
+    # Sonuç değeri görüntü üzerine yazdırılır.
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    org = (50, 50)
+    fontScale = 1
+    color = (255, 0, 0)
+    thickness = 2
+    frame = cv2.putText(frame, str(model_out.argmax().item()),
+                        org, font, fontScale, color, thickness, cv2.LINE_AA)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv2.destroyAllWindows()
+"""
